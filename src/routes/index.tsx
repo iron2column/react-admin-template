0c5dfa03c65374f5f8397714f63f5a37186877c2
陈明阳 <chenmingyang@whaty.com>
2025-06-23 11:11:58 +0800

    修改：添加错误处理页面

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
+import { lazy } from 'react'
 import Home from '@/pages/Home'
 import About from '@/pages/About'
-import NotFound from '@/pages/NotFound'
+
+const NotFound = lazy(() => import('@/pages/NotFound'))
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+    errorElement: <NotFound />,
   },
   {
     path: '/about',
