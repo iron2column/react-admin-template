e5080b18a92c57c68c115b7f3dd4d7ef7cb9448e
陈明阳 <chenmingyang@whaty.com>
2025-06-23 13:05:52 +0800

    添加：主题模式切换组件

---
+++ src/components/toggleThemeMode/SunmoonThemeMode.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@/components/ui/button'
+import { useThemeMode } from '@/lib/theme-mode-context'
+import { Sun, Moon } from 'lucide-react'
+
+export default function SunmoonThemeMode() {
+  const { themeMode, setThemeMode } = useThemeMode()
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setThemeMode(themeMode === 'dark' ? 'light' : 'dark')}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  )
+}
